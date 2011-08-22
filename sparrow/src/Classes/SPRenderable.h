ec8792d6e17ba7f31c99b3915f76841f35713254
Cory Osborn <cory@bodsix.com>
2011-08-22 16:36:56 -0700

    Adding SPRenderable protocol

---
+++ sparrow/src/Classes/SPRenderable.h
@@ -0,0 +1,14 @@
+//
+//  SPRenderable.h
+//  beastie
+//
+//  Created by Cory Osborn on 8/22/11.
+//  Copyright 2011 BodSix, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SPRenderSupport.h"
+
+@protocol SPRenderable <NSObject>
+-(void)render:(SPRenderSupport*)support;
+@end
